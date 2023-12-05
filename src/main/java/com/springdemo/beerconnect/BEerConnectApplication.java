590ab6fc9cef0c545c2243e0dd51d0647131420b
cricci3 <cricci3@studenti.uninsubria.it>
2023-12-05 12:25:47 +0100

    commit 1 prova

---
+++ src/main/java/com/springdemo/beerconnect/BEerConnectApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.beerconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BEerConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BEerConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdemo/beerconnect/BEerConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.beerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BEerConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
