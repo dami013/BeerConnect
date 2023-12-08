b16a8eced66a7d9859d25e0eca5677372177db09
Claudio Ricci <c.ricci19@campus.unimib.it>
2023-12-08 16:39:03 +0000

    Delete ClientRepository.java

--- src/main/java/com/beerconnect/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.beerconnect.repository;
-
-import com.beerconnect.assignment.ClientRecord;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<ClientRecord, String> {
-
-}
