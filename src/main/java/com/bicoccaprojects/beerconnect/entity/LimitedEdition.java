a2911d816cf01ef6ad4e49f9d8a4d6b630d73299
cricci3 <cricci3@studenti.uninsubria.it>
2023-12-15 18:59:26 +0100

    LimitedEdition column

--- src/main/java/com/bicoccaprojects/beerconnect/entity/LimitedEdition.java
+++ src/main/java/com/bicoccaprojects/beerconnect/entity/LimitedEdition.java
@@ -1,13 +1,17 @@
 package com.bicoccaprojects.beerconnect.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 
 @DiscriminatorValue("limited")
 public class LimitedEdition extends Beer{
-    String limitedEditionName;
-    int productionYear;
+    @Column(name = "le_beer", nullable = false, columnDefinition = "TEXT", unique = true)
+    private String limitedEditionName;
 
-    public LimitedEdition(String name_beer, String type, String aroma, Double alcohol, String color, String country, String ingredients, Float price, Integer quantityInStock, String limitedEditionName, int productionYear) {
+    @Column(name = "production_year", nullable = false, columnDefinition = "INTEGER", unique = true)
+    private Integer productionYear;
+
+    public LimitedEdition(String name_beer, String type, String aroma, Double alcohol, String color, String country, String ingredients, Float price, Integer quantityInStock, String limitedEditionName, Integer productionYear) {
         super(name_beer, type, aroma, alcohol, color, country, ingredients, price, quantityInStock);
         this.limitedEditionName = limitedEditionName;
         this.productionYear = productionYear;
@@ -24,11 +28,11 @@ public class LimitedEdition extends Beer{
         this.limitedEditionName = limitedEditionName;
     }
 
-    public int getProductionYear() {
+    public Integer getProductionYear() {
         return productionYear;
     }
 
-    public void setProductionYear(int productionYear) {
+    public void setProductionYear(Integer productionYear) {
         this.productionYear = productionYear;
     }
 
