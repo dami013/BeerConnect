ed399d1c3431d69fefa90dd535e3920f5e09e42f
cricci3 <cricci3@studenti.uninsubria.it>
2023-12-15 16:33:24 +0100

    modificato com.bicoccaprojects...

--- src/main/java/com/bicoccaprojects/beerconnect/BeerconnectApplication.java
+++ src/main/java/com/bicoccaprojects/beerconnect/BeerconnectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.example.testproject")
+@EntityScan(basePackages = "com.bicoccaprojects.beerconnect")
 public class BeerconnectApplication {
 
 	public static void main(String[] args) {
