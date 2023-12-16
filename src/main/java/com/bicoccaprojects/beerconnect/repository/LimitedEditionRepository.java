ed7f90dcd30e2dfbf6998e1fc66d392a8fe4b0f1
Damiano Ficara <damianoficara@gmail.com>
2023-12-16 16:39:39 +0100

    implements logic with service class

--- src/main/java/com/bicoccaprojects/beerconnect/repository/LimitedEditionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bicoccaprojects.beerconnect.repository;
-
-import com.bicoccaprojects.beerconnect.entity.LimitedEdition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LimitedEditionRepository extends JpaRepository<LimitedEdition, Long> {
-}
