7befaa070ba16803c7652fa4e24163cdbeeab0a4
Damiano Ficara <damianoficara@gmail.com>
2024-01-05 15:39:39 +0100

    change costructors

--- src/main/java/com/bicoccaprojects/beerconnect/entity/LimitedEdition.java
+++ src/main/java/com/bicoccaprojects/beerconnect/entity/LimitedEdition.java
@@ -17,8 +17,9 @@ public class LimitedEdition extends Beer{ // class that extend Beer, a LimitedEd
 
     // constructors
 
-    public LimitedEdition(String nameBeer, Pub idPub, String type, String aroma, Double alcohol, String color, String country, String ingredients, Float price, Integer quantityInStock, String originalBeer, Integer productionYear) {
-        super(nameBeer, idPub, type, aroma, alcohol, color, country, ingredients, price, quantityInStock);
+    public LimitedEdition(String nameBeer,String type, String aroma, Double alcohol, String color, String country, String ingredients, Float price,
+                          Integer quantityInStock, String originalBeer, Integer productionYear,Pub idPub) {
+        super(nameBeer, type, aroma, alcohol, color, country, ingredients, price, quantityInStock,idPub);
         this.originalBeer = originalBeer;
         this.productionYear = productionYear;
     }
