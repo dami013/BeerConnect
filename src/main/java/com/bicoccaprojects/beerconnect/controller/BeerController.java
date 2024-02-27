99b13624b2241f2acdf7fa6be6e55ecb0fb808e8
Damiano Ficara <damianoficara@gmail.com>
2024-02-27 18:46:37 +0100

    add thymeleaf

--- src/main/java/com/bicoccaprojects/beerconnect/controller/BeerController.java
+++ src/main/java/com/bicoccaprojects/beerconnect/controller/BeerController.java
@@ -1,18 +1,14 @@
 package com.bicoccaprojects.beerconnect.controller;
 
 import com.bicoccaprojects.beerconnect.entity.Beer;
-import com.bicoccaprojects.beerconnect.exception.beer.NoBeersFoundException;
 import com.bicoccaprojects.beerconnect.service.BeerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-import java.util.Optional;
+import java.security.Principal;
+
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -22,11 +18,14 @@ public class BeerController {
     @Autowired
     private BeerService beerService;
 
-    @GetMapping("/beers")
-    public String getAllBeers(Model model) {
-        Iterable<Beer> beers = beerService.getAllBeers();
-        model.addAttribute("beers", beers);
-        return "beer";
+    @GetMapping("/lista")
+    public ModelAndView  getAllBeers() {
+        ModelAndView model = new ModelAndView();
+        Iterable<Beer> lebirre = beerService.getAllBeers();
+        model.addObject("birre", lebirre);
+        model.setViewName("beers.html");
+        return model;
     }
+
 
 }
