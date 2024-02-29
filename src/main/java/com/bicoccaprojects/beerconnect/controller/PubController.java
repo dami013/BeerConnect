fc16a116eaa25ed1aec700a55febe49460b7c8c9
cricci3 <cricci3@studenti.uninsubria.it>
2024-02-29 14:17:27 +0100

    PubController

--- src/main/java/com/bicoccaprojects/beerconnect/controller/PubController.java
+++ src/main/java/com/bicoccaprojects/beerconnect/controller/PubController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,8 +25,12 @@ public class PubController {
     }
 
     @GetMapping("/pubs")
-    public Iterable<Pub> getAllPub(){
-        return pubService.getAllPubs();
+    public ModelAndView getAllPub(){
+        ModelAndView model = new ModelAndView();
+        Iterable<Pub> pubs = pubService.getAllPubs();
+        model.addObject("pubs", pubs);
+        model.setViewName("pubs");
+        return model;
     }
 
     @GetMapping("/pub")
